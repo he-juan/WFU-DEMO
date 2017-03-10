630f1b7b8cda290502d77c6476e5dd600e9d8c8f
gsbuilder <mjli@grandstream.com>
2017-03-10 09:42:46 +0800

    create bin dir before copy sox to avoid copy failed issue
    Change-Id: I9ebc42dc03634e271c34e2054b25756da0a4b9bc

--- Android.mk
+++ Android.mk
@@ -13,6 +13,7 @@ LOCAL_POST_PROCESS_COMMAND += $(shell cp -r $(LOCAL_PATH)/webgui $(TARGET_OUT)/.
 LOCAL_POST_PROCESS_COMMAND += $(shell cp -r $(LOCAL_PATH)/webserver/pem/gxe50xx.pem $(TARGET_OUT)/etc/.)
 
 LOCAL_POST_PROCESS_COMMAND := $(shell mkdir -p $(TARGET_OUT)/lib)
+LOCAL_POST_PROCESS_COMMAND += $(shell mkdir -p $(TARGET_OUT)/bin)
 LOCAL_POST_PROCESS_COMMAND += $(shell cp -r $(LOCAL_PATH)/sox/bin/sox $(TARGET_OUT)/bin/.)
 LOCAL_POST_PROCESS_COMMAND += $(shell cp -r $(LOCAL_PATH)/sox/lib/sox/lib/libsox.so* $(TARGET_OUT)/lib/.)
 LOCAL_POST_PROCESS_COMMAND += $(shell cp -r $(LOCAL_PATH)/sox/lib/libmad/lib/libmad.so $(TARGET_OUT)/lib/.)
