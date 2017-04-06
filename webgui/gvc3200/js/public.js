880cd9ed375c46805caeaa66fe27ce7d5733938a
yjzhou <yjzhou@grandstream.cn>
2017-04-06 11:04:40 +0800

    [NBF] ldap page can be saved when tapping "enter" on web Cause: None Solution: None Fixed Version: 2017.04.06 11:04 Branch: GVC3210
    Change-Id: I0722b6181fb60b4398479aed97205bfcf28de24f
    Reviewed-on: https://192.168.120.246/60633
    Reviewed-by: ChaoChaoMa <cchma@grandstream.cn>
    Tested-by: ChaoChaoMa <cchma@grandstream.cn>

--- webgui/gvc3200/js/public.js
+++ webgui/gvc3200/js/public.js
@@ -92,7 +92,7 @@ $(function(){
         if(e.keyCode == '13') {
 			if($("#jqibox").length != 0)
 				$("#jqibox").remove();
-		    if( $.cookie( "Mainpage") != "apps" ){
+		    if( $.cookie( "Mainpage") != "apps" || ($.cookie( "Mainpage") == "apps" && $.cookie("Subpage") == "ldap")){
                 $("#a_save").click();
 			    return false;
 			}else{
