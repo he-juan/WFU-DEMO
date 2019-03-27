5d8d58da603cc6b7548350807e42a453e5981fda
cchma <cchma@grandstream.cn>
2019-03-27 20:11:13 +0800

    [INTERNAL] Fixed lighttpd can not support ssl issue.
    Cause: None
    Solution: Add -DHAVE_LIBSSL flag.
    Fixed Version: 2019.03.27
    Branch: apl
    
    Change-Id: I5e3cca5015c9bd237fb5070ab674284883b8e981
    Reviewed-on: https://192.168.120.246/131687
    Reviewed-by: ChaoChaoMa <cchma@grandstream.cn>
    Tested-by: ChaoChaoMa <cchma@grandstream.cn>

--- webserver/lighttpd-1.4.32/envlighttpdarmEABI.sh
+++ webserver/lighttpd-1.4.32/envlighttpdarmEABI.sh
@@ -10,7 +10,7 @@ export CROSS_COMPILE=1
 export ARCH=arm
 
 export CFLAGS="-pie -fPIE\
-    -DHAVE_PCRE_H=1 -DBUILD_ON_ARM -DHAVE_LIBPCRE=1 -DHAVE_LOCALTIME_R=1 -DHAVE_MMAP=1 \
+    -DHAVE_PCRE_H=1 -DBUILD_ON_ARM -DHAVE_LIBSSL -DHAVE_LIBPCRE=1 -DHAVE_LOCALTIME_R=1 -DHAVE_MMAP=1 \
     -I${ANDROID_ROOT}/vendor/intel/external/openssl/include \
     -I${ANDROID_ROOT}/external/libxml2/include \
     -I${ANDROID_ROOT}/external/dbus \
@@ -31,8 +31,9 @@ export LDFLAGS="-nostdlib -pie -fPIE --sysroot=${ANDROID_ROOT}/prebuilts/ndk/r10
     -L${ANDROID_ROOT}/out/target/product/leaf_hill/obj/lib64/ \
     -L${ANDROID_ROOT}/out/target/product/leaf_hill/obj/lib/ \
     -L${ANDROID_ROOT}/out/target/product/leaf_hill/system/lib64 \
+    -L${ANDROID_ROOT}/out/target/product/leaf_hill/vendor/lib64 \
     -L${ANDROID_ROOT}/prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.9/lib/gcc/x86_64-linux-android/4.8 \
     -L${ANDROID_ROOT}/prebuilts/ndk/r10/platforms/android-18/arch-x86_64/usr/lib64 "
 
-export LIBS="-lc -ldl -lm -lssl -lcrypto -lnvram -ldbus -llog -licuuc -lpcrecpp -lsqlite -lwebupdate -lcutils \
+export LIBS="-lc -ldl -lm -lssl2 -lcrypto2 -lnvram -ldbus -llog -licuuc -lpcrecpp -lsqlite -lwebupdate -lcutils \
     ${ANDROID_ROOT}/out/target/product/leaf_hill/obj/STATIC_LIBRARIES/libxml2_intermediates/libxml2.a"
