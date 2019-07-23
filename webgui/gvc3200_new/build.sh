c3e38430fb1e835c12d75e9f64520ea818c16f1e
cchma <cchma@grandstream.cn>
2019-07-23 17:57:08 +0800

    [INTERNAL] update the auto build script, set the registry to local npm server: http://192.168.120.239:8080.
    Cause: None
    Solution: None
    Fixed Version: 2019.07.23 17:57:08
    Branch: H51
    
    Change-Id: Id5192d807f944220a49a243ca28fbae673c03901
    Reviewed-on: https://192.168.120.246/149080
    Reviewed-by: ChaoChaoMa <cchma@grandstream.cn>
    Tested-by: ChaoChaoMa <cchma@grandstream.cn>

--- webgui/gvc3200_new/build.sh
+++ webgui/gvc3200_new/build.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 build_path=`pwd`
 if ! [ -x "$(command -v node)" ]; then
-  echo '检测到node尚未安装。正在试图从网络安装...所需时间与你的网络环境有关'
-  cd /usr/local/etc
-  path=`pwd`
-  wget https://nodejs.org/dist/v10.9.0/node-v10.9.0-linux-x64.tar.xz -T 60 -t 2
-  tar xf  node-v10.9.0-linux-x64.tar.xz 
-  ln -s $path/node-v10.9.0-linux-x64/bin/npm   /usr/local/bin/npm
-  ln -s $path/node-v10.9.0-linux-x64/bin/node   /usr/local/bin/node
-  cd $build_path	 
-fi
-if ! [ -x "$(command -v node)" ]; then
-  echo 'node自动安装失败,建议你手动安装好node环境后再启动本脚本' 
-  exit 1
-fi
-if ! [ -x "$(command -v npm)" ]; then
-  echo 'npm自动安装失败,建议你手动安装npm' 
+  echo '检测到node/npm还未安装, 请先安装node/npm后再执行自动编译'
   exit 1
 fi
 
-npm install --unsafe-perm=true --allow-root
+npm install --unsafe-perm --registry=http://192.168.120.239:8080
 
 npm run product
-
-# 请使用root用户执行此脚本
