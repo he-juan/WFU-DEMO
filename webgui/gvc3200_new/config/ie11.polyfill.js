8b853dd26040528628469184762fdbc300110360
yplu <yplu@grandstream.cn>
2019-11-15 11:03:08 +0800

    [INTERNAL] remove the condition to judge ie11 when polyfill, chome 53 doesnt support Object.values
    Cause: None
    Solution: None
    Fixed Version: 20191115
    Branch: GVC3220
    
    Change-Id: Idafcd26df8a1bf2dd5b923b3fb5ecf586ddcebcb
    Reviewed-on: https://192.168.120.246/165838
    Reviewed-by: leiliu <leiliu@grandstream.cn>
    Tested-by: leiliu <leiliu@grandstream.cn>

--- webgui/gvc3200_new/config/ie11.polyfill.js
+++ webgui/gvc3200_new/config/ie11.polyfill.js
@@ -1,5 +1,3 @@
 // ie 11 polyfill ie 11 兼容
-if (!!window.ActiveXObject || 'ActiveXObject' in window) {
-  require('react-app-polyfill/ie11')
-  require('react-app-polyfill/stable')
-}
+require('react-app-polyfill/ie11')
+require('react-app-polyfill/stable')
