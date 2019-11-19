68a3dcd6041b3ccc34e7ae072e752ce64eb969cd
Lei Liu <leiliu@grandstream.cn>
2019-11-19 17:34:38 +0800

    [INTERNAL] Fix codes for FormItem Component
    Cause: None
    Solution: None
    Fixed Version: 20191119 17:34
    Branch: GVC3220
    Change-Id: I66637e7cf035242738bf1998ab5c7a504c182f6d
    Reviewed-on: https://192.168.120.246/166375
    Reviewed-by: yplu <yplu@grandstream.cn>
    Tested-by: yplu <yplu@grandstream.cn>

---
+++ webgui/gvc3200_new/src/pages/Backend/Account/VideoConf/BlueJeans/GeneralSettings.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import { Form, Button } from 'antd'
+import FormCommon from '@/components/FormCommon'
+import { connect } from 'react-redux'
+import { getOptions } from '@/template'
+import FormItem, { CheckboxItem, InputItem } from '@/components/FormItem'
+import DefaultAcctModal from '@/components/DefaultAcctModal'
+import { $t } from '@/Intl'
+
+@connect(
+  state => ({
+    acctStatus: state.acctStatus,
+    defaultAcct: state.defaultAcct
+  })
+)
+@Form.create()
+class GeneralSettings extends FormCommon {
+  options = getOptions('Account.VideoConf.BlueJeans.General')
+  state = {
+    restActiveAcct: []
+  }
+  componentDidMount () {
+    const { setFieldsValue } = this.props.form
+    this.initFormValue(this.options).then(data => {
+      setFieldsValue(data)
+    })
+  }
+
+  // 提交表单
+  handleSubmit = () => {
+    const { validateFields } = this.props.form
+    const { acctStatus, defaultAcct } = this.props
+    validateFields((err, values) => {
+      if (!err) {
+        let restActiveAcct = acctStatus.filter(acct => acct.acctIndex !== 2 && acct.activate === 1)
+        if ((values['P501'] === 0 && defaultAcct === 2) && restActiveAcct.length > 0) {
+          this.setState({
+            restActiveAcct: restActiveAcct
+          })
+          return false
+        }
+        this.submitFormValue(values, 1)
+      }
+    })
+  }
+  render () {
+    const { restActiveAcct } = this.state
+    const { getFieldDecorator: gfd } = this.props.form
+    const options = this.options
+    return (
+      <Form>
+        {/* 帐号激活 */}
+        <CheckboxItem
+          {...options['P501']}
+          gfd={gfd}
+        />
+        {/* 显示名称 */}
+        <InputItem
+          {...options['P507']}
+          gfd={gfd}
+          gfdOptions={{
+            rules: [
+              this.maxLen(64)
+            ]
+          }}
+        />
+        <FormItem label='' >
+          <Button className='sub-btn' onClick={this.handleSubmit} id='subBtn'>{$t('b_001')}</Button>
+        </FormItem>
+        <DefaultAcctModal restActiveAcct={restActiveAcct} cb={() => this.handleSubmit()} onCancel={() => this.setState({ restActiveAcct: [] })}/>
+      </Form>
+    )
+  }
+}
+
+export default GeneralSettings
