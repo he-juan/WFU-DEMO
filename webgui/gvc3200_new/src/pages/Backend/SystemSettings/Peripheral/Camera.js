9faceb512b37bd9c798512c4b47c2944ab715063
Lei Liu <leiliu@grandstream.cn>
2020-03-13 14:02:07 +0800

    [BugFree 81563] WEB cannot set custom initial camera position
    Cause: None
    Solution: None
    Fixed Version: 20200313 14:00
    Branch: GVC3220
    Change-Id: I78b15bb83024095dfec7833894cab7b02a9da12b
    Reviewed-on: https://192.168.120.246/182045
    Reviewed-by: leiliu <leiliu@grandstream.cn>
    Tested-by: leiliu <leiliu@grandstream.cn>

--- webgui/gvc3200_new/src/pages/Backend/SystemSettings/Peripheral/Camera.js
+++ webgui/gvc3200_new/src/pages/Backend/SystemSettings/Peripheral/Camera.js
@@ -1,19 +1,36 @@
 import React from 'react'
 import FormCommon from '@/components/FormCommon'
 import FormItem, { SelectItem, SliderItem } from '@/components/FormItem'
-import { Form, Button } from 'antd'
+import { Form, Button, message } from 'antd'
 import { getOptions } from '@/template'
 import { $t } from '@/Intl'
+import API from '@/api'
 
 @Form.create()
 class Camera extends FormCommon {
   options = getOptions('System.Peripheral.Camera')
+
+  state = {
+    presetInfo: []
+  }
 
   componentDidMount () {
     const { getFieldsValue, setFieldsValue } = this.props.form
     this.initFormValue(this.options).then(data => {
       const fileds = getFieldsValue()
       Object.keys(fileds).length > 0 && setFieldsValue(data)
+    })
+    this.getPresetInfo()
+  }
+
+  // 获取预置位列表
+  getPresetInfo = () => {
+    API.getPresetInfo().then(res => {
+      if (res.Response === 'success') {
+        this.setState({ presetInfo: res.Data })
+      } else {
+        message.error(res.Message || $t('m_071'))
+      }
     })
   }
 
@@ -29,6 +46,19 @@ class Camera extends FormCommon {
   render () {
     const { getFieldDecorator: gfd } = this.props.form
     const options = this.options
+    const { presetInfo } = this.state
+
+    const positions = [
+      { v: '0', t: $t('c_022') },
+      { v: '100', t: $t('c_023') }
+    ]
+    presetInfo.forEach(item => {
+      const { position, name } = item
+      if (+position < 24) {
+        const s = (+position + 1).toString()
+        positions.push({ v: s, t: name || $t('c_274') + s })
+      }
+    })
 
     return (
       <Form>
@@ -43,10 +73,7 @@ class Camera extends FormCommon {
         <SelectItem
           gfd={gfd}
           {...options['P25030']}
-          selectOptions={[
-            { v: '0', t: $t('c_022') },
-            { v: '1', t: $t('c_023') }
-          ]}
+          selectOptions={positions}
         />
         <FormItem>
           <Button className='sub-btn' id='subBtn' onClick={this.handleSubmit}>{$t('b_001')}</Button>
