7ff552512ab99d616102985646c55031c499b5a7
cchma <cchma@grandstream.cn>
2020-03-25 11:49:03 +0800

    [INTERNAL] send WS message to WebUI when contacts/calllog database changed.
    Cause: WebUI can know when to refresh the contacts and calllogs data by this.
    Solution: None
    Fixed Version: 2020.03.25
    Branch: GVC3220
    
    Change-Id: Iefd71ca24eab537843aee2649d6cff95c62e700b
    Reviewed-on: https://192.168.120.246/183980
    Reviewed-by: ChaoChaoMa <cchma@grandstream.cn>
    Tested-by: ChaoChaoMa <cchma@grandstream.cn>

--- webserver/websocket/tcpserver.c
+++ webserver/websocket/tcpserver.c
@@ -88,6 +88,8 @@
 #define CONF_STATUS_CHANGED "confstatuschanged"
 #define DND_STATE_CHANGED "dndstatuschanged"
 #define PRESENTATION_STATUS_CHANGED_DBUS "presentaionstatuschanged"
+#define CONTACTS_UPDATED_DBUS "contacts_updated"
+#define CALLLOG_UPDATED_DBUS "calllog_updated"
 
 static char *dbus_path = "/com/grandstream/dbus/webservice";
 static char *dbus_dest = "com.grandstream.dbus.gmi.server";
@@ -1377,6 +1379,44 @@ static DBusHandlerResult signal_filter2 (DBusConnection *dbconnection, DBusMessa
             memset(sendData, 0, len);
             snprintf(sendData, len, "{\"type\":\"presentaion-status-changed\", \"data\":%s},", str);
             LOGD("PRESENTATION_CHANGED_DBUS: %s", sendData);
+            sendDataToSocket(sendData);
+            free(sendData);
+        }
+        else
+        {
+            printf("receive freeline message error: %s\n", error.message);
+            dbus_error_free(&error);
+        }
+    }
+    else if (dbus_message_is_signal(message, DBUS_INTERFACE_WEB, CONTACTS_UPDATED_DBUS))
+    {
+        if (dbus_message_get_args(message, &error,
+                                  DBUS_TYPE_INVALID))
+        {
+            len = 128;
+            sendData = malloc(len);
+            memset(sendData, 0, len);
+            snprintf(sendData, len, "{\"type\":\"contacts_updated\"},");
+            LOGD("CONTACTS_UPDATED_DBUS: %s", sendData);
+            sendDataToSocket(sendData);
+            free(sendData);
+        }
+        else
+        {
+            printf("receive freeline message error: %s\n", error.message);
+            dbus_error_free(&error);
+        }
+    }
+    else if (dbus_message_is_signal(message, DBUS_INTERFACE_WEB, CALLLOG_UPDATED_DBUS))
+    {
+        if (dbus_message_get_args(message, &error,
+                                  DBUS_TYPE_INVALID))
+        {
+            len = 128;
+            sendData = malloc(len);
+            memset(sendData, 0, len);
+            snprintf(sendData, len, "{\"type\":\"calllog_updated\"},");
+            LOGD("CALLLOG_UPDATED_DBUS: %s", sendData);
             sendDataToSocket(sendData);
             free(sendData);
         }
