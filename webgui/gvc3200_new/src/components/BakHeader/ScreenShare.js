a16e3b4d44cc57d86fb05617cb0f7e7890b39732
yplu <yplu@grandstream.cn>
2020-04-07 10:04:51 +0800

    [INTERNAL] update Screen Share :  call the  "call" and "beginScreen" with one step
    Cause: None
    Solution: None
    Fixed Version: 20200407
    Branch: GVC3220
    
    Change-Id: Ife4e4a0fc9aed08ce557b99a37f0fd5f07b9ec9b
    Reviewed-on: https://192.168.120.246/185781
    Reviewed-by: yplu <yplu@grandstream.cn>
    Tested-by: yplu <yplu@grandstream.cn>

--- webgui/gvc3200_new/src/components/BakHeader/ScreenShare.js
+++ webgui/gvc3200_new/src/components/BakHeader/ScreenShare.js
@@ -2,37 +2,39 @@
 import React, { Component } from 'react'
 import { message } from 'antd'
 import { $t } from '@/Intl'
+import { connect } from 'react-redux'
+import { setWholeLoading } from '@/store/actions'
 
 const GS_RTC = window.GS_RTC
 
+@connect(
+  state => ({}),
+  dispatch => ({
+    setWholeLoading: (isLoad, tips) => dispatch(setWholeLoading(isLoad, tips))
+  })
+)
 class ScreenShare extends Component {
   state = {
-    isSharing: false,
-    rtc_enabled: true
+    isCalled: false, // 是否call了
+    isCalling: false, // 是否正在call
+    isSharing: false, // 是否正在share
+    rtc_enabled: true // 屏幕共享是否可用
   }
 
   errorCode = null
 
   componentDidMount () {
     setTimeout(() => {
-      GS_RTC.CALL(this.getURL(), res => {
-        console.log('CALL************************' + res.codeType + '**********************')
-        if (res.codeType != 200) {
-          this.setState({
-            rtc_enabled: false
-          })
-          this.errorCode = res.codeType
-        }
-      })
-
       console.log('gsRTC ******************************', window.gsRTC)
 
-      window.gsRTC.on('stopShareScreen', () => {
+      window.gsRTC.on('stopShareScreen', (res) => {
+        console.log('STOP_SCREEN ************************' + res.codeType + '**********************')
         this.setState({
           isSharing: false
         })
       })
-      window.gsRTC.on('shareScreen', () => {
+      window.gsRTC.on('shareScreen', (res) => {
+        console.log('BEGIN_SCREEN ************************' + res.codeType + '**********************')
         this.setState({
           isSharing: true
         })
@@ -41,14 +43,44 @@ class ScreenShare extends Component {
   }
 
   getURL = () => {
-    const host = process.env.NODE_ENV === 'development' ? '192.168.125.108' : window.location.host
+    const host = process.env.NODE_ENV === 'development' ? '192.168.124.191' : window.location.host
     const protocol = window.location.protocol
     return protocol.indexOf('https:') !== -1 ? `wss://${host}/websockify` : `ws://${host}/websockify`
   }
 
+  // 首次点击 调用call
+  handleCall = () => {
+    this.setState({
+      isCalling: true
+    })
+    this.props.setWholeLoading(true, $t('m_063'))
+    GS_RTC.CALL(this.getURL(), res => {
+      this.props.setWholeLoading(false, '')
+      console.log('CALL************************' + res.codeType + '**********************')
+      if (res.codeType != 200) {
+        this.setState({
+          rtc_enabled: false,
+          isCalled: true,
+          isCalling: false
+        })
+        message.error(res.codeType)
+        this.errorCode = res.codeType
+      } else {
+        this.setState({
+          isCalled: true,
+          isCalling: false
+        })
+        setTimeout(() => {
+          this.handleClick()
+        }, 1000)
+      }
+    })
+  }
+
+  // call 完成后 调用 beginScreen
   handleStartShare = () => {
     GS_RTC.BEGIN_SCREEN((res) => {
-      console.log('BEGIN_SCREEN ************************' + res.codeType + '**********************')
+      // console.log('BEGIN_SCREEN ************************' + res.codeType + '**********************')
       if (res.codeType == 200) {
       } else {
         this.handleStopShare()
@@ -56,9 +88,10 @@ class ScreenShare extends Component {
     })
   }
 
+  // 调用stopScreen
   handleStopShare = () => {
     GS_RTC.STOP_SCREEN((res) => {
-      console.log('STOP_SCREEN ************************' + res.codeType + '**********************')
+      // console.log('STOP_SCREEN ************************' + res.codeType + '**********************')
       if (res.codeType == 200) {
         this.setState({
           isSharing: false
@@ -71,11 +104,26 @@ class ScreenShare extends Component {
     if (navigator.mediaDevices === undefined || navigator.mediaDevices.enumerateDevices === undefined) {
       return message.error($t('m_262')) // 共享屏幕功能需要开启https.
     }
-    if (!this.state.rtc_enabled) {
+
+    let { isSharing, rtc_enabled, isCalled, isCalling } = this.state
+
+    if (isCalling) {
+      return false
+    }
+
+    // 是否 调用过call了， 第一次点击需求调用 call, call返回200后 再次执行handleClick
+    if (!isCalled) {
+      this.handleCall()
+      return false
+    }
+
+    // rtc_enabled 根据 call返回是否200确定共享屏幕功能是否可用， 如果不可用 报错返回
+    if (!rtc_enabled) {
       message.error(this.errorCode)
       return false
     }
-    let { isSharing } = this.state
+
+    // 如果正在开启共享屏幕功能， 则点击后执行暂停
     if (isSharing) {
       this.handleStopShare()
     } else {
