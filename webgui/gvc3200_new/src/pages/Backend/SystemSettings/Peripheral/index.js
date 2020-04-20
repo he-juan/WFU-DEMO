8f3e079fe819694f52ebe2d439516b2300e9bc7a
yplu <yplu@grandstream.cn>
2020-04-20 17:39:28 +0800

    [INTERNAL] hide page 'WirelessMic', 'Media'
    Cause: None
    Solution: None
    Fixed Version: 20200420
    Branch: GVC3220
    
    Change-Id: I47ffb04eb93ce14ef3e4004c502e70249af91b0d
    Reviewed-on: https://192.168.120.246/188025
    Reviewed-by: yplu <yplu@grandstream.cn>
    Tested-by: yplu <yplu@grandstream.cn>

--- webgui/gvc3200_new/src/pages/Backend/SystemSettings/Peripheral/index.js
+++ webgui/gvc3200_new/src/pages/Backend/SystemSettings/Peripheral/index.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react'
 import TabPages from '@/components/TabPages'
 import HDMI from './HDMI' // HDMI
 import Camera from './Camera' // 摄像头
-import WirelessMic from './WirelessMic' // 无线麦 r_076 待做
-import Media from './Media' // Media r_077 待做
+// import WirelessMic from './WirelessMic' // 无线麦 r_076 待做
+// import Media from './Media' // Media r_077 待做
 
 const routes = [
   { tab: 'r_074', path: '/manage/sys_peripheral/hdmi', component: HDMI },
-  { tab: 'r_075', path: '/manage/sys_peripheral/camera', component: Camera },
-  { tab: 'r_076', path: '/manage/sys_peripheral/wirelessmic', component: WirelessMic },
-  { tab: 'r_077', path: '/manage/sys_peripheral/media', component: Media }
+  { tab: 'r_075', path: '/manage/sys_peripheral/camera', component: Camera }
+  // { tab: 'r_076', path: '/manage/sys_peripheral/wirelessmic', component: WirelessMic },
+  // { tab: 'r_077', path: '/manage/sys_peripheral/media', component: Media }
 ]
 
 class Security extends Component {
